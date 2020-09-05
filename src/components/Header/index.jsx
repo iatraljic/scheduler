40268000fc544a31c6816db095d61e90607b2f04
iatraljic <iatraljic@gmail.com>
2020-09-05 09:00:39 +0200

    Added navigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {
+  MainContext,
+  NEXT_DAY,
+  PREV_DAY,
+  NEXT_WEEK,
+  PREV_WEEK,
+} from '../../context';
 
-function CalendarHeader() {
+function Header() {
+  const { dispatch } = useContext(MainContext);
+
   return (
     <div className="calendar-header">
-      <button>Dan</button>
+      <button onClick={() => dispatch({ type: PREV_WEEK })}>⮜⮜</button>
+      <button onClick={() => dispatch({ type: PREV_DAY })}>⮜</button>
+      <button onClick={() => dispatch({ type: NEXT_DAY })}>⮞</button>
+      <button onClick={() => dispatch({ type: NEXT_WEEK })}>⮞⮞</button>
     </div>
   );
 }
 
-export default CalendarHeader;
+export default Header;
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,20 +1,35 @@
 import React, { createContext, useReducer } from 'react';
 
 export const SET_FIRST_DAY = 'SET_FIRST_DAY';
-export const NEXT_DAY = 'NEXT_DAY';
 export const PREV_DAY = 'PREV_DAY';
-export const NEXT_WEEK = 'NEXT_WEEK';
+export const NEXT_DAY = 'NEXT_DAY';
 export const PREV_WEEK = 'PREV_WEEK';
+export const NEXT_WEEK = 'NEXT_WEEK';
 
 const initialState = {
   firstDay: null,
 };
 
 function reducer(state, action) {
+  const manipulationDay = new Date(state.firstDay);
+
   switch (action.type) {
     case SET_FIRST_DAY:
       return { firstDay: action.payload };
+    case PREV_DAY:
+      manipulationDay.setDate(manipulationDay.getDate() - 1);
+      return { firstDay: manipulationDay };
+    case NEXT_DAY:
+      manipulationDay.setDate(manipulationDay.getDate() + 1);
+      return { firstDay: manipulationDay };
+    case PREV_WEEK:
+      manipulationDay.setDate(manipulationDay.getDate() - 7);
+      return { firstDay: manipulationDay };
+    case NEXT_WEEK:
+      manipulationDay.setDate(manipulationDay.getDate() + 7);
+      return { firstDay: manipulationDay };
     default:
+      // ne želimo action koji nismo definirali
       throw new Error();
   }
 }
